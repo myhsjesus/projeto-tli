ffec74c30ce76ffb90cae0e5ced1ce42b78436b1
Mylena Santos <jmylena.moinho@gmail.com>
2023-11-10 16:42:16 -0300

    Atualizações da hom e da destinos

---
+++ js/busca1.js
@@ -0,0 +1,9 @@
+const passageiros = document.getElementById('passageiros');
+const origem = document.querySelector("#origem");
+const destino = document.querySelector("#destino");
+const data = document.querySelector("#data");
+const pacotes = [];
+
+function suggest(origem, destino, data){
+    
+}
